257888b89b162330564f7dc05ca6779dc878c699
FoxtrotLimaDelta <61523717+FoxtrotLimaDelta@users.noreply.github.com>
2020-10-25 19:29:55 -0700

    Earthquake maps code -circleMarkers working

---
+++ static/js/logic.js
@@ -0,0 +1,58 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+  // Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  console.log(data);
+
+  L.geoJSON(data,{
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  }).addTo(myMap);
+
+});
+
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+function geojsonFeature() {
+  var myLayer = L.geoJSON().addTo(myMap);
+  myLayer.addData(geojsonFeature);
+}
+
+var geojsonFeature = {
+  "type": "Feature",
+  "properties": {
+      "title": "",
+      "mag": "",
+      "detail": ""
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": []
+  }
+};
