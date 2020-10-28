9b4b7959e6fb3b18ae3ea18b7d38fef1be5cfdec
FoxtrotLimaDelta <61523717+FoxtrotLimaDelta@users.noreply.github.com>
2020-10-27 23:40:10 -0700

    Completed Earthquake Maps in Leaflet - Level 1

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -8,7 +8,39 @@ var myMap = L.map("map", {
   zoom: 5
 });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+//==========================
+// Define arrays to hold created city and state markers
+var cityMarkers = [];
+var stateMarkers = [];
+
+// Loop through locations and create city and state markers
+for (var i = 0; i < locations.length; i++) {
+  // Setting the marker radius for the state by passing population into the markerSize function
+  stateMarkers.push(
+    L.circle(locations[i].coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "white",
+      radius: markerSize(locations[i].state.population)
+    })
+  );
+
+  // Setting the marker radius for the city by passing population into the markerSize function
+  cityMarkers.push(
+    L.circle(locations[i].coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "purple",
+      fillColor: "purple",
+      radius: markerSize(locations[i].city.population)
+    })
+  );
+}
+//=====================================
+// Create base layers
+// Streetmap Layer
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -17,6 +49,42 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+//===============
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+// Create two separate layer groups: one for cities and one for states
+var states = L.layerGroup(stateMarkers);
+var cities = L.layerGroup(cityMarkers);
+
+// Create a baseMaps object
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+
+// Create an overlay object
+var overlayMaps = {
+  "State Population": states,
+  "City Population": cities
+};
+
+// Define a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [streetmap, states, cities]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
 
 function determineColor(mag) {
   if ( mag > 5) {
@@ -41,7 +109,7 @@ function onEachFeature(feature, layer) {
       mouseover: function(event) {
         layer = event.target;
         layer.setStyle({
-          fillOpacity: 5.9
+          fillOpacity: 0.9
         });
       },
 
@@ -52,7 +120,11 @@ function onEachFeature(feature, layer) {
           fillOpacity: 0.5
         });
       },
-      });
+      // When a feature (earthquake) is clicked, it is enlarged to fit the screen
+      click: function(event) {
+        myMap.fitBounds(event.target.getBounds());
+      }
+     });
 // // Giving each feature a pop-up with information pertinent to it
 layer.bindPopup("<h1>" + (feature.properties.place) + "</h1>" + "<hr>" + "<h2>" + "Magnitude" + (feature.properties.mag) + "</h2>");
 }
@@ -96,3 +168,5 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(myMap);
+
+
